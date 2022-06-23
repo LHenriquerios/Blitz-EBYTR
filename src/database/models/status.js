e2a6a0953b5f894544aef13167da57d2db682889
LHenriquerios <henriquerios4@gmail.com>
2022-06-23 13:46:18 -0300

    feat: create models

---
+++ src/database/models/status.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+    const Status = sequelize.define('Status', {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        type: DataTypes.STRING,
+    },
+    {
+        timestamps: false,
+    });
+
+    Status.associate = (models) => {
+        Status.hasMany(models.Task, { foreignKey: 'statusId', as: 'tasks' })
+    };
+
+    return Status;
+};
