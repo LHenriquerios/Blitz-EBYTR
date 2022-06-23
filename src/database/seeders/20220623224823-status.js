55ebb4482068ce7c61e7c713f560eaf397fe5da0
LHenriquerios <henriquerios4@gmail.com>
2022-06-23 20:02:00 -0300

    feat: add seeders

---
+++ src/database/seeders/20220623224823-status.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, _Sequelize) {
+
+    await queryInterface.bulkInsert('Status', [{
+      id: 1,
+      type: 'editável',
+    },
+    {
+      id: 2,
+      type: 'pendente',
+    },
+    {
+      id: 3,
+      type: 'em andamento',
+    },
+    {
+      id: 4,
+      type: 'pronto',
+    }], { timestemps: false });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Status', null, {});
+  }
+};
