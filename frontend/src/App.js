3c302ac830789f9d93c650a9576b25da757397e3
LHenriquerios <henriquerios4@gmail.com>
2022-07-03 15:56:16 -0300

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [tasks, setTasks] = useState([]);
+  
+  useEffect(() => {
+
+    async function fetchData () {
+      try{
+    const response =  await fetch('http://localhost:3001/tasks');
+    const data = response.json()
+    console.log(data);
+    setTasks(data);
+    }
+      catch (error) {
+        console.log(error);
+  }
+}
+  }, []);
+
   return (
-    <div className="App">
-      <h1>start</h1>
-    </div>
+    <>
+      <ul>
+        {console.log(tasks)}
+        {tasks.map(task => (
+          <><li key={task.id}>{`Tarefa: ${task.contents}`}</li>
+          <li key={task.id}>{`Status: ${task.status}`}</li><br/></>
+        ))}
+      </ul>
+    </>
   );
 }
 
